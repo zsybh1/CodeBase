42b6feef855876c3eb41ed62ab41f336d11c28ce
zsybh1 <zsybh1@163.com>
2021-05-15 10:06:08 +0800

    iterator

--- cpp/iterator.cpp
+++ cpp/iterator.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main () {
     // 流迭代器批量输入
-    istream_iterator<int> in(cin), eof;
+    istream_iterator<int> in(cin), eof; // cin也可以替换成fstream
     vector<int> vec(in, eof);
 
-    for (auto i : vec) {
-        cout << i << " ";
-    }
+    // 流迭代器批量输出
+    ostream_iterator<int> out(cout, " ");
+    copy(vec.cbegin(), vec.cend(), out);
+    cout << endl;   // 刷新输出流
 }
